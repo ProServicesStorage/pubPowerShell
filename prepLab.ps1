1b48f874002fafc36470bce2b2ef4ad14f9d78fc
Mark Richardson <proservicesstorage@gmail.com>
2020-04-26 10:18:32 -0700

    Another Commit

---
+++ prepLab.ps1
@@ -0,0 +1,37 @@
+
+#The following Powershell example script preps server and is used for deployment
+
+#Functions
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+function Disable-UserAccessControl {
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000
+    Write-Host "User Access Control (UAC) has been disabled." -ForegroundColor Green    
+}
+
+#Main
+
+#Disable Windows Firewall
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+#Install IIS
+Install-WindowsFeature -Name Web-Server
+#Install .NET 3.5
+Install-WindowsFeature Net-Framework-Core
+
+#Disable UAC and Internet Exploere
+Disable-UserAccessControl
+Disable-InternetExplorerESC
+
+#Disable Windows Defender AV Realtime Monitoring
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+#Download and install Chrome
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
+
